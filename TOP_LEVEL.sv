c8a3603043aea3183b8ef3e2702b42ae6acd6a55
Young He <vrownie@ucla.edu>
2023-12-14 18:05:18 -0800

    finish top level with 2 PEs (lol)

--- TOP_LEVEL.sv
+++ TOP_LEVEL.sv
@@ -3,26 +3,32 @@
 module top (
     input clk, reset, valid,
 	input [`DATA_SIZE_ARB-1:0] q, data_i, twiddle_i,
-    input [`DATA_SIZE_ARB-1:0] data_o,
+    output reg [`DATA_SIZE_ARB-1:0] data_o,
+    output reg is_done
 );
+
+    `define ADDR_SIZE $clog2(`RING_SIZE)
 
     logic ram1_re, ram2_re, done;
     logic sel_a1, sel_a2, sel_ram_redundant;
     logic [`DATA_SIZE_ARB-1:0] bit_rev_o, top_ram_o, bot_ram_o;
-    logic [`DATA_SIZE_ARB-1:0] pe1_o, pe2_0;
+    logic [`DATA_SIZE_ARB-1:0] pe1_o, pe2_o;
     logic [11:0] count = 0;
     logic last_stage;
 
     
     //Signals for to and bottom RAMs
     logic we_top, we_bot, addgen_wr; 
-    logic [$clog2(`RING_SIZE)-1:0] wr_addr_br, rd_addr_br; //Signals from bit reverse
-    logic [$clog2(`RING_SIZE)-1:0] wr_addr_addgen, rd_addr_addgen; //Signals from address generator
-    logic [$clog2(`RING_SIZE)-1:0] rd_addr, wr_addr;
+    logic [`ADDR_SIZE-1:0] wr_addr_br, rd_addr_br; //Signals from bit reverse
+    logic [`ADDR_SIZE-1:0] wr_addr_addgen, rd_addr_addgen; //Signals from address generator
+    logic [`ADDR_SIZE-1:0] rd_addr, wr_addr;
     logic [`DATA_SIZE_ARB-1:0] top_ram_i, bot_ram_i;
 
     //Final Stage
     logic [`DATA_SIZE_ARB-1:0] last_pe_top, last_pe_bot;
+    logic [`ADDR_SIZE-1:0] reorder_wr_addr_top, reorder_wr_addr_bot, reorder_rd_addr;
+    logic in_done, out_done;
+    logic [`DATA_SIZE_ARB-1:0] last_ram_top_o, last_ram_bot_o;
 
     bit_reverse BR (
         .clk(clk), .reset(reset), .valid(valid),
@@ -31,7 +37,7 @@ module top (
         .addr(br_addr)
     );
 
-    AddressGenerator2 AddrGen(.clk(clk), .rst(reset), done.(done), .rdAddress(rd_addr_addgen),
+    AddressGenerator2 AddrGen(.clk(clk), .rst(reset), .done(done), .rdAddress(rd_addr_addgen),
                              .wrAddress(wr_addr_addgen), .wrValid(addgen_wr), .last_statge());
 
     contoller control( 
@@ -91,23 +97,42 @@ module top (
     PE_Tilde final_PE( 
         .clk(clk), .reset(reset),
         .q(q), .data_top_i(pe1_o), .data_bot_i(pe2_o), 
-        .ntt_top_o(last_pe_top), ntt_bot_o(last_pe_bot)
+        .ntt_top_o(last_pe_top), .ntt_bot_o(last_pe_bot)
     );
 
-    Reorder_Out #(3)
-    last_ram(
+    Reorder_Out #(ADDR_SIZE) reorder(
         .clk(clk), .reset(reset), .next_pair(last_stage), 
-        .wr_addr_top(), .wr_addr_bot(), .rd_addr(), 
-        .in_done(), .out_done()
+        .wr_addr_top(reorder_wr_addr_top), 
+        .wr_addr_bot(reorder_wr_addr_bot), 
+        .rd_addr(reorder_rd_addr), 
+        .in_done(in_done), .out_done(out_done)
+    );
+
+    // manually create 2-write 1-read BRAM with MSB toggling
+    BRAM #(`DATA_SIZE_ARB, `RING_SIZE/2) 
+    last_ram_top(
+        .clk(clk), .wen(1'b1),
+        .waddr(reorder_wr_addr_top[`ADDR_SIZE-2:0]), .din(last_pe_top),
+        .raddr(reorder_rd_addr[`ADDR_SIZE-2:0]), .dout(top_ram_o)
     );
 
     BRAM #(`DATA_SIZE_ARB, `RING_SIZE/2) 
-    last_ram(
-        .clk(clk), .wen( ),
-        .waddr( ), .din( ),
-        .raddr(rd_addr_addgen), .dout(bot_ram_o)
+    last_ram_bot(
+        .clk(clk), .wen(1'b1),
+        .waddr(reorder_wr_addr_bot[`ADDR_SIZE-2:0]), .din(last_pe_bot),
+        .raddr(reorder_rd_addr[`ADDR_SIZE-2:0]), .dout(bot_ram_o)
     );
 
+    // mux out of 2 BRAMs
+    always @* begin
+        if (reorder_rd_addr[`ADDR_SIZE-1]) begin
+            data_o = bot_ram_o;
+        end else begin
+            data_o = top_ram_o;
+        end
+    end
+
+    assign is_done = out_done;
     
 endmodule
 
